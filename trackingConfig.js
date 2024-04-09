ad0069f6065a244410de475bf147f8813db2189e
video-arts <162715833+video-arts@users.noreply.github.com>
2024-04-09 13:09:07 +0100

    Add files via upload

---
+++ trackingConfig.js
@@ -0,0 +1,13 @@
+var TrackingConfig = {
+    "type" : "none",
+    "success_status": "passed",
+    "unsuccess_status": "failed",
+    "send_score":"true",
+    "send_status":"true",
+    "send_interactions":"true",
+    "send_objectives":"true",
+    "mastery_score": "80",
+    "appversion"  : "2.3.0" 
+     
+     
+    };
